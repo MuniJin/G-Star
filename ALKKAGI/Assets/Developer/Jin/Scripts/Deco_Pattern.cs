9e0054b6ff129da633398ddd5a1b4822c81a2fac
jsm <cokesoda2@naver.com>
2023-09-06 16:41:18 +0900

    데코2

--- ALKKAGI/Assets/Developer/Jin/Scripts/Deco_Pattern.cs
+++ ALKKAGI/Assets/Developer/Jin/Scripts/Deco_Pattern.cs
@@ -2,75 +2,142 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Base_Player
+public abstract class Base_Unit
 {
-    // hp delay speed name cooldown
-    protected int Hp;
-    protected float Cooldown;
-    protected string Name;
+    public string Name = string.Empty;
+    public int Hp = 100;
+    public float Speed = 5f;
+    public float Shoot_Delay = 0.5f;
+    public float Skill_CoolDown = 3f;
 
-    public abstract string Get_All_Information();
-    public abstract int Get_Hp();
-    public abstract float Get_Cooldown();
-    public abstract string Get_Name();
+    public virtual void Get_All_Information() { Debug.Log("Test"); }
 }
 
-public class InitDeco : Base_Player
+public class Player_Unit : Base_Unit
 {
-    public InitDeco()
+    public Player_Unit()
     {
-        this.Hp = 0;
-        this.Cooldown = 0f;
-        this.Name = "Init";
+        this.Name = "Player";
     }
-    public override string Get_All_Information()
+
+    public void Move()
     {
-        return $"Hp : {this.Hp} | CoolDown : {this.Cooldown} | Name : {this.Name}";
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("asdf");
+        }
     }
-    public override int Get_Hp() { return this.Hp; }
-    public override float Get_Cooldown() { return this.Cooldown; }
-    public override string Get_Name() { return this.Name; }
+    public void Jump() { }
+    public void Shoot() { }
 }
 
-
-public class Pawn : InitDeco
+public class AI_Unit : Base_Unit
 {
-    public Pawn()
+    public AI_Unit()
     {
-        this.Hp = 100;
-        this.Cooldown = 3.0f;
+        this.Name = "AI Enemy";
+    }
+
+    public void Move() { }
+    public void Jump() { }
+    public void Shoot() { }
+}
+
+/////////////////////////////////////////////////////////////////
+#region À¯´Ö µ¥ÄÚ
+public abstract class Decorator_Unit : Base_Unit
+{
+    protected Base_Unit b_unit;
+}
+
+// ÂÌ
+public class Pawn : Decorator_Unit
+{
+    public Pawn(Base_Unit b)
+    {
+        this.b_unit = b;
         this.Name = "Pawn";
+        this.Hp += 10;
+    }
+
+    public override void Get_All_Information()
+    {
+        Debug.Log($"{this.b_unit} | {this.Name} | {this.Hp}");
     }
 }
 
+// ¸¶
+public class Knight : Decorator_Unit
+{
+    public Knight(Base_Unit b)
+    {
+        this.b_unit = b;
+        this.Name = "Knight";
+    }
+}
 
+// »ó 
+public class Elephant : Decorator_Unit
+{
+    public Elephant(Base_Unit b)
+    {
+        this.b_unit = b;
+        this.Name = "Elephant";
+    }
+}
+
+// Æ÷ 
+public class Cannon : Decorator_Unit
+{
+    public Cannon(Base_Unit b)
+    {
+        this.b_unit = b;
+        this.Name = "Cannon";
+    }
+}
+
+// Â÷ 
+public class Rook : Decorator_Unit
+{
+    public Rook(Base_Unit b)
+    {
+        this.b_unit = b;
+        this.Name = "Rock";
+    }
+}
+
+// »ç 
+public class Guards : Decorator_Unit
+{
+    public Guards(Base_Unit b)
+    {
+        this.b_unit = b;
+        this.Name = "Guards";
+    }
+}
+
+// ¿Õ
+public class King : Decorator_Unit
+{
+    public King(Base_Unit b)
+    {
+        this.b_unit = b;
+        this.Name = "King";
+    }
+}
+#endregion
 
 public class Deco_Pattern : MonoBehaviour
 {
-    private Base_Player b;
+    public GameObject go;
+    public Base_Unit b;
 
-    public void Start()
+    private void Start()
     {
-        b = new InitDeco();
-    }
+        go.AddComponent<Player_Unit>();
+        //Base_Unit b = new Player_Unit();
 
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q) && b.Get_Name() != "Pawn")
-            Deco_Pawn();
-        else if (Input.GetKeyDown(KeyCode.W) && b.Get_Name() != "Init")
-            Deco_Init();
-    }
-
-    private void Deco_Pawn()
-    {
-        b = new Pawn();
-        Debug.Log(b.Get_All_Information());
-    }
-
-    private void Deco_Init()
-    {
-        b = new InitDeco();
-        Debug.Log(b.Get_All_Information());
+        //b = new Pawn(b);
+        //b.Get_All_Information();
     }
 }
