23538a0a6d8bf0b7827f52ad87d25b3b8ec571dc
sys <111103274+samgirlsss@users.noreply.github.com>
2023-10-23 19:24:40 +0900

    Ui

--- ALKKAGI/Assets/Developer/Song/SYS/script/testskilui.cs
+++ ALKKAGI/Assets/Developer/Song/SYS/script/testskilui.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class testskilui : MonoBehaviour
 {
-    public Image img_Skill;
+    public Image fill;
 
     // Use this for initialization
     void Start()
@@ -29,10 +29,43 @@ public class testskilui : MonoBehaviour
         while (cool > 1.0f)
         {
             cool -= Time.deltaTime;
-            img_Skill.fillAmount = (1.0f / cool);
+            fill.fillAmount = (1.0f/cool );
             yield return new WaitForFixedUpdate();
         }
 
         print("쿨타임 코루틴 완료");
     }
 }
+//public UnityEngine.UI.Image fill;
+//private float maxCooldown = 6f;
+//private float currentCooldown = 6f;
+
+//public void SetMaxCooldown(in float value)
+//{
+//    maxCooldown = value;
+//    UpdateFiilAmount();
+//}
+
+//public void SetCurrentCooldown(in float value)
+//{
+//    currentCooldown = value;
+//    UpdateFiilAmount();
+//}
+
+//private void UpdateFiilAmount()
+//{
+//    fill.fillAmount = currentCooldown / maxCooldown;
+//}
+
+//// Test
+//private void Update()
+//{
+
+//    SetCurrentCooldown(currentCooldown - Time.deltaTime);
+
+//    // Loop
+//    if (currentCooldown < 0f)
+//        currentCooldown = maxCooldown;
+
+
+//}
